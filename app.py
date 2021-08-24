84e7ab420f046bf39343a0ca24b572182d4e48f2
Eger37 <dan37sugak@gmail.com>
2021-08-24 15:05:37 +0300

    backend start

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask, send_from_directory
-from flask_restful import Api, Resource, reqparse
-from flask_cors import CORS #comment this on deployment
-from api.HelloApiHandler import HelloApiHandler
+from flask import Flask, request
+from flask_restful import Resource,  Api
+from flask_jwt_extended import JWTManager
+from flask_jwt_extended import create_access_token, jwt_required
+from flask_cors import CORS
 
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
-CORS(app) #comment this on deployment
+app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'my_cool_secret'
+jwt = JWTManager(app)
+CORS(app)
 api = Api(app)
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
 
-api.add_resource(HelloApiHandler, '/flask/hello')
+class UserLogin(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        if username == 'admin' and password == 'habr':
+            access_token = create_access_token(identity={
+                'role': 'admin',
+            }, expires_delta=False)
+            result = {'token': access_token}
+            return result
+        return {'error': 'Invalid username and password'}
+
+
+class ProtectArea(Resource):
+    @jwt_required
+    def get(self):
+        return {'answer': 42}
+
+
+api.add_resource(UserLogin, '/api/login/')
+api.add_resource(ProtectArea, '/api/protect-area/')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
