5205119e09e100fe0d04086a79adc72d03a630d5
Eger37 <dan37sugak@gmail.com>
2021-09-14 22:35:54 +0300

    remove todos

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ from flask_restful import reqparse, abort, Api, Resource
 from flask_jwt_extended import JWTManager
 from flask_jwt_extended import jwt_required
 from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, send, disconnect, emit, join_room, leave_room, rooms
+
 from api.api_admin import AdminLogin, AdminUsersList
+
 
 app = Flask(__name__)
 
@@ -13,20 +16,32 @@ CORS(app)
 
 api = Api(app)
 
-TODOS = [{"id": 0, "name": 0, "title": " Анна Каренина "},
-         {"id": 1, "name": 0, "title": " Война и мир "},
-         {"id": 2, "name": 1, "title": " Гордость и предубеждение "},
-         {"id": 3, "name": 1, "title": " Гордость и предубеждение "},
-         {"id": 4, "name": 1, "title": " Разум и чувствительность "}]
-
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
+users_list = [{"id": 0, "name": "Никита12"},
+              {"id": 1, "name": "Ники6та5"},
+              {"id": 2, "name": "Ники6та4"},
+              {"id": 3, "name": "Ники6та35"},
+              {"id": 4, "name": "Ники6та92"},
+              {"id": 5, "name": "Никита856"},
+              {"id": 6, "name": "Никита467"},
+              {"id": 7, "name": "Никита316"},
+              {"id": 8, "name": "Ник6ита262"},
+              {"id": 9, "name": "Никита222"},
+              {"id": 10, "name": "Н6икита12"},
+              {"id": 11, "name": "Никита51"},
+              {"id": 12, "name": "Ни6кита41"},
+              {"id": 13, "name": "Ник6ита315"},
+              {"id": 14, "name": "Никита912"},
+              {"id": 15, "name": "Ник6ита185"},
+              {"id": 16, "name": "Никита471"},
+              {"id": 17, "name": "Ники6та311"},
+              {"id": 18, "name": "Никита122"},
+              {"id": 19, "name": "Олег315"},
+              {"id": 20, "name": "Виктор912"},
+              {"id": 21, "name": "Валера185"},
+              {"id": 22, "name": "Никалераита471"},
+              {"id": 23, "name": "Никиалера6та311"},
+              {"id": 24, "name": "Ниалеракита122"},
+              {"id": 25, "name": "Никалераи6та122"}]
 
 
 class ProtectArea(Resource):
@@ -34,55 +49,6 @@ class ProtectArea(Resource):
     def get(self):
         return {'answer': 42}
 
-
-# Todo
-# shows a single todo item and lets you delete a todo item
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-
-# TodoList
-# shows a list of all todos, and lets you POST to add new tasks
-class TodoList(Resource):
-    def get(self):
-        response = jsonify(TODOS)
-        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
-        # response.headers.add('Access-Control-Allow-Origin', ' http://localhost:3001')
-        # response.headers.add('content-type', 'application/json')
-        # response.headers.add('Access-Control-Allow-Headers', 'content-range')
-        response.headers.add('Access-Control-Expose-Headers', 'content-range')
-        response.headers.add('content-range', 5)
-        return response
-        # return TODOS, 200, {'content-type': 'application/json', 'content-range': 5}
-
-    def post(self):
-        args = parser.parse_args()
-        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
-        todo_id = 'todo%i' % todo_id
-        TODOS[todo_id] = {'task': args['task']}
-        return TODOS[todo_id], 201
-
-
-# api.add_resource(TodoList, '/todos')
-# api.add_resource(Todo, '/todos/<todo_id>')
-
-
-# api.add_resource(TodoList, '/api/admin/users')
-
-api.add_resource(Todo, '/todos/<todo_id>')
 
 api.add_resource(AdminLogin, '/api/admin/login/')
 
