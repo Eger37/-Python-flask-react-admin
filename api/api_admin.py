227fa1d4e266e8abc3412cc476db7c0991229c64
Eger37 <dan37sugak@gmail.com>
2021-09-14 13:45:31 +0300

    add sort list add normal list cutting

--- api/api_admin.py
+++ api/api_admin.py
@@ -1,41 +1,91 @@
 from flask import request, jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse, abort
 from flask_jwt_extended import create_access_token
+
+from operator import itemgetter
 
 users_list = [{"id": 0, "name": "Никита12"},
               {"id": 1, "name": "Ники6та5"},
               {"id": 2, "name": "Ники6та4"},
-              {"id": 3, "name": "Ники6та35"}, ]
+              {"id": 3, "name": "Ники6та35"},
+              {"id": 4, "name": "Ники6та92"},
+              {"id": 5, "name": "Никита856"},
+              {"id": 6, "name": "Никита467"},
+              {"id": 7, "name": "Никита316"},
+              {"id": 8, "name": "Ник6ита262"},
+              {"id": 9, "name": "Никита222"},
+              {"id": 10, "name": "Н6икита12"},
+              {"id": 11, "name": "Никита51"},
+              {"id": 12, "name": "Ни6кита41"},
+              {"id": 13, "name": "Ник6ита315"},
+              {"id": 14, "name": "Никита912"},
+              {"id": 15, "name": "Ник6ита185"},
+              {"id": 16, "name": "Никита471"},
+              {"id": 17, "name": "Ники6та311"},
+              {"id": 18, "name": "Никита122"},
+              {"id": 19, "name": "Олег315"},
+              {"id": 20, "name": "Виктор912"},
+              {"id": 21, "name": "Валера185"},
+              {"id": 22, "name": "Никалераита471"},
+              {"id": 23, "name": "Никиалера6та311"},
+              {"id": 24, "name": "Ниалеракита122"},
+              {"id": 25, "name": "Никалераи6та122"}]
 
 
-# {"id": 4, "name": "Ники6та92"},
-# {"id": 5, "name": "Никита856"},
-# {"id": 6, "name": "Никита467"},
-# {"id": 7, "name": "Никита316"},
-# {"id": 8, "name": "Ник6ита262"},
-# {"id": 9, "name": "Никита222"},
-# {"id": 10, "name": "Н6икита12"},
-# {"id": 11, "name": "Никита51"},
-# {"id": 12, "name": "Ни6кита41"},
-# {"id": 13, "name": "Ник6ита315"},
-# {"id": 14, "name": "Никита912"},
-# {"id": 15, "name": "Ник6ита185"},
-# {"id": 16, "name": "Никита471"},
-# {"id": 17, "name": "Ники6та311"},
-# {"id": 18, "name": "Никита122"},
-# {"id": 19, "name": "Ники6та122"}],
+def abort_if_todo_doesnt_exist(user_id):
+    if user_id not in users_list:
+        abort(404, message="users_list {} doesn't exist".format(user_id))
 
 
-def parse_list():
-    print()
+parser = reqparse.RequestParser()
+parser.add_argument('range')
+parser.add_argument('filter')
+parser.add_argument('sort')
 
 
-def content_range(list_response):
+def format_list(list_response):
+    args = parser.parse_args()
     length = len(list_response)
-    print(length)
 
-    str_content_range = f'0-5/{length}'
-    return str_content_range
+    filter_of_list = {'filter': args['filter']}
+    sort_of_list = args['sort'].replace('[', '').replace(']', '').replace('\"', '').split(',')
+    sort_key = sort_of_list[0]
+    order = False
+    if sort_of_list[1] != 'ASC':
+        order = True
+
+    list_response = sorted(list_response, key=itemgetter(sort_key), reverse=order)
+
+    range_of_list = args['range'].replace('[', '').replace(']', '').split(',')
+    section_start, section_end = tuple(map(int, range_of_list))
+    section_of_list = list_response[section_start: section_end]
+
+    # range_of_list
+    # {'range': '[10,19]'}
+    # filter_of_list
+    # {'filter': '{}'}
+    # sort_of_list
+    # {'sort': '["name","DESC"]'}
+
+    print("filter_of_list")
+    print(filter_of_list)
+
+    print("sort_of_list")
+    print(sort_of_list)
+
+    print(section_of_list)
+
+    str_content_range = f'{section_start}-{section_end}/{length}'
+    return str_content_range, section_of_list
+
+
+# def content_range(list_response):
+#     # length = len(list_response)
+#     length = 15
+#     # print(length)
+#
+#     str_content_range = f'0-5/{length}'
+#     return str_content_range
 
 
 class AdminLogin(Resource):
@@ -53,13 +103,15 @@ class AdminLogin(Resource):
 
 class AdminUsersList(Resource):
     def get(self):
-        response = jsonify(users_list)
+        str_content_range, section_of_list = format_list(users_list)
+        response = jsonify(section_of_list)
         response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
         # response.headers.add('Access-Control-Allow-Origin', ' http://localhost:3001')
         # response.headers.add('content-type', 'application/json')
         # response.headers.add('Access-Control-Allow-Headers', 'content-range')
         response.headers.add('Access-Control-Expose-Headers', 'content-range')
-        str_content_range = content_range(users_list)
+
+        # str_content_range = content_range(users_list)
         response.headers.add('content-range', f'users {str_content_range}')
         return response
 
--- frontend-admin/src/dataProvider.jsx
+++ frontend-admin/src/dataProvider.jsx
@@ -1,9 +1,10 @@
-// import {fetchUtils} from 'react-admin'
+import {fetchUtils} from 'react-admin'
 // import axios from 'axios'
-// import {stringify} from 'query-string'
+import {stringify} from 'query-string'
+
 const apiUrl = 'http://localhost:5000/api/admin'
 
-// const httpClient = fetchUtils.fetchJson
+const httpClient = fetchUtils.fetchJson
 
 // const httpClient = (url, options = {}) => {
 //     if (!options.headers) {
@@ -33,18 +34,16 @@ export default {
     // },
     //
     getList: (resource, params) => {
-        console.log(params)
-        // const {page, perPage} = params.pagination
-        // const {field, order} = params.sort
-        // const query = {
-        //     sort: JSON.stringify([field, order]),
-        //     range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-        //     filter: JSON.stringify(params.filter),
-        // }
-
-        // const url = `${apiUrl}/${resource}?${stringify(query)}`
-
-        const url = `${apiUrl}/${resource}/`
+        // console.log(params)
+        const {page, perPage} = params.pagination
+        const {field, order} = params.sort
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        }
+        // console.log(query)
+        const url = `${apiUrl}/${resource}?${stringify(query)}`
 
         // console.log(url)
 
@@ -64,8 +63,8 @@ export default {
                 return response.json()
             })
             .then((data) => {
-                console.log(data)
-                console.log(headers.get('content-range'))
+                // console.log(data)
+                // console.log(headers.get('content-range'))
                 return {
                     data: data,
                     total: parseInt(headers.get('content-range').split('/').pop(), 10),
@@ -74,73 +73,73 @@ export default {
     },
 
 
-    // getOne: (resource, params) =>
-    //     httpClient(`${apiUrl}/${resource}/${params.id}`).then(({json}) => ({
-    //         data: json,
-    //     })),
-    //
-    // getMany: (resource, params) => {
-    //     const query = {
-    //         filter: JSON.stringify({id: params.ids}),
-    //     }
-    //     const url = `${apiUrl}/${resource}?${stringify(query)}`
-    //     return httpClient(url).then(({json}) => ({data: json}))
-    // },
-    //
-    // getManyReference: (resource, params) => {
-    //     const {page, perPage} = params.pagination
-    //     const {field, order} = params.sort
-    //     const query = {
-    //         sort: JSON.stringify([field, order]),
-    //         range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
-    //         filter: JSON.stringify({
-    //             ...params.filter,
-    //             [params.target]: params.id,
-    //         }),
-    //     }
-    //     const url = `${apiUrl}/${resource}?${stringify(query)}`
-    //
-    //     return httpClient(url).then(({headers, json}) => ({
-    //         data: json,
-    //         total: parseInt(headers.get('content-range').split('/').pop(), 10),
-    //     }))
-    // },
-    //
-    // update: (resource, params) =>
-    //     httpClient(`${apiUrl}/${resource}/${params.id}`, {
-    //         method: 'PUT',
-    //         body: JSON.stringify(params.data),
-    //     }).then(({json}) => ({data: json})),
-    //
-    // updateMany: (resource, params) => {
-    //     const query = {
-    //         filter: JSON.stringify({id: params.ids}),
-    //     }
-    //     return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
-    //         method: 'PUT',
-    //         body: JSON.stringify(params.data),
-    //     }).then(({json}) => ({data: json}))
-    // },
-    //
-    // create: (resource, params) =>
-    //     httpClient(`${apiUrl}/${resource}`, {
-    //         method: 'POST',
-    //         body: JSON.stringify(params.data),
-    //     }).then(({json}) => ({
-    //         data: {...params.data, id: json.id},
-    //     })),
-    //
-    // delete: (resource, params) =>
-    //     httpClient(`${apiUrl}/${resource}/${params.id}`, {
-    //         method: 'DELETE',
-    //     }).then(({json}) => ({data: json})),
-    //
-    // deleteMany: (resource, params) => {
-    //     const query = {
-    //         filter: JSON.stringify({id: params.ids}),
-    //     }
-    //     return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
-    //         method: 'DELETE',
-    //     }).then(({json}) => ({data: json}))
-    // }
+    getOne: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({json}) => ({
+            data: json,
+        })),
+
+    getMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({id: params.ids}),
+        }
+        const url = `${apiUrl}/${resource}?${stringify(query)}`
+        return httpClient(url).then(({json}) => ({data: json}))
+    },
+
+    getManyReference: (resource, params) => {
+        const {page, perPage} = params.pagination
+        const {field, order} = params.sort
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify({
+                ...params.filter,
+                [params.target]: params.id,
+            }),
+        }
+        const url = `${apiUrl}/${resource}?${stringify(query)}`
+
+        return httpClient(url).then(({headers, json}) => ({
+            data: json,
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        }))
+    },
+
+    update: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({json}) => ({data: json})),
+
+    updateMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({id: params.ids}),
+        }
+        return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({json}) => ({data: json}))
+    },
+
+    create: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}`, {
+            method: 'POST',
+            body: JSON.stringify(params.data),
+        }).then(({json}) => ({
+            data: {...params.data, id: json.id},
+        })),
+
+    delete: (resource, params) =>
+        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'DELETE',
+        }).then(({json}) => ({data: json})),
+
+    deleteMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({id: params.ids}),
+        }
+        return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
+            method: 'DELETE',
+        }).then(({json}) => ({data: json}))
+    }
 }
