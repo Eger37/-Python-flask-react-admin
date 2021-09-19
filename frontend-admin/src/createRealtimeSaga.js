bd3ef7349bd17e257a6e16404ac8661f8d712286
Eger37 <dan37sugak@gmail.com>
2021-09-19 20:34:00 +0300

    ra-realtime saga

---
+++ frontend-admin/src/createRealtimeSaga.js
@@ -0,0 +1,35 @@
+import realtimeSaga from 'ra-realtime'
+
+const observeRequest = dataProvider => (type, resource, params) => {
+    // Filtering so that only users are updated in real time
+    if (resource !== 'users') return
+
+    // Use your apollo client methods here or sockets or whatever else including the following very naive polling mechanism
+    return {
+        subscribe(observer) {
+            const intervalId = setInterval(() => {
+                dataProvider(type, resource, params)
+                    .then(results => observer.next(results)) // New data received, notify the observer
+                    .catch(error => observer.error(error)) // Ouch, an error occured, notify the observer
+            }, 5000)
+
+            const subscription = {
+                unsubscribe() {
+                    // Clean up after ourselves
+                    clearInterval(intervalId)
+                    // Notify the saga that we cleaned up everything
+                    observer.complete()
+                }
+            }
+
+            return subscription
+        },
+    }
+}
+
+
+function realtimeProvider(dataProvider) {
+    realtimeSaga(observeRequest(dataProvider))
+}
+
+export default realtimeProvider
--- frontend-admin/src/main.jsx
+++ frontend-admin/src/main.jsx
@@ -1,19 +1,21 @@
 import * as React from "react"
 import {Admin, Resource} from 'react-admin'
-import Dashboard from './Dashboard'
-import authProvider from './authProvider'
-import {UserList} from './users'
 import UserIcon from '@material-ui/icons/Group'
+
+import Dashboard from './Dashboard'
+import {UserList} from './users'
+
+import authProvider from './authProvider'
+import createRealtimeSaga from "./createRealtimeSaga"
 import dataProvider from "./dataProvider"
 
+const realTimeSaga = createRealtimeSaga(dataProvider)
+
 const Main = () => (
-    <div>
-        <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-            <Resource name="users" list={UserList} icon={UserIcon}/>
-        </Admin>
-    </div>
-
-
+    <Admin dashboard={Dashboard} authProvider={authProvider}
+           dataProvider={dataProvider} customSagas={[realTimeSaga]}>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
+    </Admin>
 )
 
 export default Main
