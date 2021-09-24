280c604cf3c7b9579d2e87f59894644c3128e2ba
Eger37 <dan37sugak@gmail.com>
2021-09-24 18:21:40 +0300

    refactoring docker

--- docker.frontend.start.sh
+++ docker.frontend.start.sh
@@ -3,8 +3,6 @@ app="docker.edt1.frontend"
 docker build -t ${app} -f DockerfileFrontend .
 docker run --rm -d -p 3001:3001 ${app}
 
-
-
 #sudo bash docker.frontend.start.sh
 
 #docker run --rm  -p 3001:3001 docker.edt1.frontend
